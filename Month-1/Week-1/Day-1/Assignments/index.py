068236e5b8c19cf6ae5b71fbb7a3b2fc8e8a3e18
mohantysuraj925-star <mohantysuraj925@gmail.com>
2026-06-10 19:23:34 +0530

    added python print tasks and fixed html

---
+++ Month-1/Week-1/Day-1/Assignments/index.py
@@ -0,0 +1,10 @@
+# Task 1: Print your name
+print("Suraj Kumar Mohanty")
+
+# Task 2: Print your college name
+print("Gandhi Institute of Excellent Technocrats, Ganga Patna, Bhubaneswar")
+
+# Task 3: Print 3 lines using print()
+print("I am pursuing a Diploma in Computer Science Engineering.")
+print("My goal is to become an ethical hacker and cybersecurity expert.")
+print("Currently, I am learning Python programming and web development.")
